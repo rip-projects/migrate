a917d7b77969539d86654b96d2fb25946c54b21d
Ganesha <reekoheek@gmail.com>
2014-06-20 19:12:00 +0700

    fixing template

--- templates/_migrate/rollback.php
+++ templates/_migrate/rollback.php
@@ -1,21 +1,23 @@
-<h2>Rollback</h2>
+<div style="width:100%;">
+    <h2>Rollback</h2>
 
-<div class="row button-form">
-    <div class="span-12">
-        <div class="row">
-            <ul class="flat">
-                <li>
-                    <a href="<?php echo URL::site('/migrate/show').'?token='.$_GET['token'] ?>" class="button">Show</a>
-                </li>
-            </ul>
+    <div class="row button-form">
+        <div class="span-12">
+            <div class="row">
+                <ul class="flat">
+                    <li>
+                        <a href="<?php echo URL::site('/migrate/show').'?token='.$_GET['token'] ?>" class="button">Show</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 
-<ul>
-<?php foreach($logs as $log): ?>
-<li>
-    <pre style="margin:0;padding:0;"><code><?php echo $log ?></code></pre>
-</li>
-<?php endforeach ?>
-</ul>
+    <ul>
+    <?php foreach($logs as $log): ?>
+    <li>
+        <pre style="margin:0;padding:0;"><code><?php echo $log ?></code></pre>
+    </li>
+    <?php endforeach ?>
+    </ul>
+</div>
